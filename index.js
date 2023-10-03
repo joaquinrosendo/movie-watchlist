2bda52c52b25bc34bf577dbac4101bcc6556995c
Joaquin Rosendo <joaquinrosendo@live.com>
2023-10-03 16:24:01 -0500

    finish search movies page

--- index.js
+++ index.js
@@ -1,35 +1,73 @@
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
-const mainContainer = document.getElementsByTagName('main');
+let mainContainer = document.getElementById('main-container');
+const startExploringMessage = document.getElementById('start-exploring-message');
+const errorMessage = document.getElementById('error-search-message');
+const moviesContainer = document.getElementById('movies-container');
 
-let displayMovies = () => {
-    mainContainer.innerHtml =
-    `<div class="movie-container">
-        <img class="movie-poster"/>
-        <div class="movie-info">
-            <div class="movie-header">
-                <h3 id="movie-title"></h3>
-                <i class="fa-solid fa-star"></i>
-                <p id="movie-rating"></p>
-            </div>
-            <div class="movie-specs">
-                <p id="movie-runtime"></p>
-                <p id="movie-genre"></p>
-                <p id="add-watchlist"></p>
-            </div>
-            <p id="movie-plot-paragraph"></p>
-        </div>
-    </div>`
+let displayMovies = (movies) => {
+
+    let promises = [];
+
+    movies.forEach(movie => {
+        promises.push(
+            fetch(`http://www.omdbapi.com/?t=${movie.Title}&apikey=e2f87681`)
+            .then(res => res.json())
+            .then(data => { 
+                return `<div class="movie-container" id="movie-container">
+                                        <img class="movie-poster" src="${data.Poster}"/>
+                                        <div class="movie-info">
+                                            <div class="movie-header">
+                                                <h3 id="movie-title">${data.Title}</h3>
+                                                <i class="fa-solid fa-star"></i>
+                                                <p id="movie-rating">${data.imdbRating}</p>
+                                            </div>
+                                            <div class="movie-specs">
+                                                <p id="movie-runtime">${data.Runtime}</p>
+                                                <p id="movie-genre">${data.Genre}</p>
+                                                <a class="add-to-watchlist"><i class="fa-solid fa-circle-plus"></i>
+                                                <p id="add-watchlist">Watchlist</p></a>
+                                            </div>
+                                            <p id="movie-plot-paragraph">${data.Plot}</p>
+                                        </div>
+                                    </div>`;
+            })
+        );
+        
+    });
+
+    Promise.all(promises)
+    .then(htmlArray => {
+        let html = htmlArray.join('');
+        moviesContainer.innerHTML = html;
+        moviesContainer.style.display = 'flex'
+        startExploringMessage.style.display = 'none';
+        errorMessage.style.display = 'none'
+        console.log(promises)
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
 }
+
 
 let searchMovie = () => {
     let movie = searchInput.value;
 
-    fetch(`http://www.omdbapi.com/?t=${movie}&apikey=e2f87681`)
+    fetch(`http://www.omdbapi.com/?s=${movie}&type=movie&apikey=e2f87681`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data.Response);
+        if(data.Response === 'False'){
+            startExploringMessage.style.display = 'none';
+            moviesContainer.style.display = 'none'
+            errorMessage.style.display = 'flex'
+        }
+        else {
+            displayMovies(data.Search);
+        }
+        
+    })
 }
 
 searchBtn.addEventListener('click', searchMovie);
-
-displayMovies();
